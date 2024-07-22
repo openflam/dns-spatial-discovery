9037916812b84e6cb0f85b7d65fbdf5770be24c9
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2024-07-22 18:06:41 -0400

    Changed location-to-servers to ts

--- js/src/location-to-servers.ts
+++ js/src/location-to-servers.ts
@@ -4,7 +4,7 @@ import { LocationToGeoDomain } from './location-to-geo-domain';
 
 class LocationToServerAddr {
     // The DNS object to use for querying DNS records
-    dnsObj = null;
+    dnsObj: DNS;
 
     constructor() {
         this.dnsObj = new DNS();
@@ -12,25 +12,24 @@ class LocationToServerAddr {
 
     /**
      * 
-     * @param {Number} lat Latitude
-     * @param {Number} lon Longitude
-     * @param {Number} error_m Error in meters
-     * @param {String} suffix The suffix to append to the geo domains. Default is from the config file.
+     * @param lat Latitude
+     * @param lon Longitude
+     * @param error_m Error in meters
+     * @param suffix The suffix to append to the geo domains. Default is from the config file.
      * @returns Domains to query for the given location.
      */
-    async getServersAddrs(lat, lon, error_m, suffix = CONFIG.GEO_DOMAIN_SUFFIX) {
-        var geoDomains = LocationToGeoDomain.getGeoDomains(lat, lon, error_m, suffix);
-        var serverAddrs = [];
-        for (let domain of geoDomains) {
+    async getServersAddrs(lat: number, lon: number, error_m: number, suffix: string = CONFIG.GEO_DOMAIN_SUFFIX): Promise<string[]> {
+        const geoDomains = LocationToGeoDomain.getGeoDomains(lat, lon, error_m, suffix);
+        const serverAddrs: string[] = [];
+        for (const domain of geoDomains) {
             try {
                 const result = await this.dnsObj.dnsLookup(domain, 'TXT');
-                for (let record of result) {
+                for (const record of result) {
                     if ('data' in record) {
                         serverAddrs.push(record.data);
                     }
                 }
-            }
-            catch (error) {
+            } catch (error) {
                 console.log(error);
                 continue;
             }
