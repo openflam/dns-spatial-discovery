707e44b444cf0d84ca014f7e11b1f502d8182134
Sagar Bharadwaj <skalasib@andrew.cmu.edu>
2024-07-21 20:14:54 -0400

    Changed webpack output library to umd type. Changed output file to bundle.js (from main.js)

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -3,8 +3,11 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    library: 'dnsspatialdiscovery',
+    library: {
+      name: 'dnsspatialdiscovery',
+      type: 'umd'
+    }
   }
 };
